6be156e09bf7ffc5a94cc98b0fa3390cfea24b55
walteraandrade <walteraandrade@gmail.com>
2023-11-30 10:57:20 -0300

    removed url from codegen

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
 	overwrite: true,
 	schema: {
-		'https://instituto-playlist.hasura.app/v1/graphql': {
+		'' /* paste url from env */: {
 			headers: {
 				'x-hasura-admin-secret': /* paste key from env */ '',
 			},
