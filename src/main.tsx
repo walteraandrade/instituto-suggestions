7dee1763d76c83ca93e67a1a25a8ba7049e25d25
walteraandrade <walteraandrade@gmail.com>
2023-12-01 11:44:54 -0300

    removed apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
-const graphqlUrl = import.meta.env.VITE_GRAPHQL_ENDPOINT;
-const hasuraKey = import.meta.env.HASURA_ADMIN_SECRET;
-const client = new ApolloClient({
-	uri: graphqlUrl,
-	cache: new InMemoryCache(),
-	headers: {
-		'x-hasura-admin-secret': hasuraKey,
-	},
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<ApolloProvider client={client}>
-			<App />
-		</ApolloProvider>
+		<App />
 	</React.StrictMode>
 );
