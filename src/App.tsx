ee8090f610549f9d6b623c9de1bbf39d157aef23
RafaelPires2 <rafael.pires@taqtile.com.br>
2024-01-05 16:11:14 -0300

    feat: moved query

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,40 @@
-import { useQuery, gql } from '@apollo/client';
-import { DefaultSpinner } from './components/spinner';
-import { SuggestionsQueryQuery } from './data/schema/graphql';
-import { SuggestionCard } from './components/suggestion-card';
-import { SuggestionTitle } from './components/suggestion-title';
-import { SuggestionImage } from './components/suggestion-image';
-import { Button } from './components/button';
-import { CreateNewSuggestion } from './components/create-new-suggestion';
-import { Suspense } from 'react';
-const query = gql`
-	query SuggestionsQuery {
-		playlist {
-			author
-			created_at
-			id
-			image
-			link
-			name
-			updated_at
-		}
-	}
-`;
+import { useQuery } from "@apollo/client";
+import { DefaultSpinner } from "./components/spinner";
+import { SuggestionsQueryQuery } from "./data/schema/graphql";
+import { SuggestionCard } from "./components/suggestion-card";
+import { SuggestionTitle } from "./components/suggestion-title";
+import { SuggestionImage } from "./components/suggestion-image";
+import { Button } from "./components/button";
+import { CreateNewSuggestion } from "./components/create-new-suggestion";
+import { Suspense } from "react";
+import { suggestionsQuery } from "./data/schema/query/suggestions-query";
 
 function App() {
-	const { data } = useQuery<SuggestionsQueryQuery>(query);
+  const { data } = useQuery<SuggestionsQueryQuery>(suggestionsQuery);
 
-	return (
-		<div className="bg-gray-100 px-12 py-8 h-screen text-center">
-			<h1 className="text-2xl">Instituto's Suggestions</h1>
-			<hr />
-			<CreateNewSuggestion />
-			<div className="mt-8" />
-			<hr />
-			<div className="mt-8" />
-			<div className="flex">
-				<Suspense fallback={<DefaultSpinner />}>
-					{data?.playlist.map((suggestion) => (
-						<SuggestionCard suggestion={suggestion} key={suggestion.id}>
-							<SuggestionTitle />
-							<SuggestionImage />
-							<div className="mt-8" />
+  return (
+    <div className="bg-gray-100 px-12 py-8 h-screen text-center">
+      <h1 className="text-2xl">Instituto's Suggestions</h1>
+      <hr />
+      <CreateNewSuggestion />
+      <div className="mt-8" />
+      <hr />
+      <div className="mt-8" />
+      <div className="flex">
+        <Suspense fallback={<DefaultSpinner />}>
+          {data?.playlist.map((suggestion) => (
+            <SuggestionCard suggestion={suggestion} key={suggestion.id}>
+              <SuggestionTitle />
+              <SuggestionImage />
+              <div className="mt-8" />
 
-							<Button />
-						</SuggestionCard>
-					))}
-				</Suspense>
-			</div>
-		</div>
-	);
+              <Button />
+            </SuggestionCard>
+          ))}
+        </Suspense>
+      </div>
+    </div>
+  );
 }
 
 export default App;
