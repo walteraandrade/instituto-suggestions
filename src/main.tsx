c6006655331bfa0a1c73fccc0824428d68a7698c
RafaelPires2 <rafael.pires@taqtile.com.br>
2024-01-05 16:11:29 -0300

    feat: client apollo

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+const client = new ApolloClient({
+  uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
+  headers: {
+    "x-hasura-admin-secret": import.meta.env.VITE_HASURA_ADMIN_SECRET,
+  },
+  cache: new InMemoryCache(),
+});
+console.log(import.meta.env.VITE_HASURA_ADMIN_SECRET);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
 );
